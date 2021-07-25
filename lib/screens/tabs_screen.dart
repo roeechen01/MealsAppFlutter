af4887388769164e51e68226c6c57b2d53b13375
Roee Chen <roeechen01@gmail.com>
2021-07-25 15:21:13 +0300

    Changed tab bar type to a bottom tab bar (better looking tab bar)

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,26 +9,36 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Meal Categories'},
+    {'page': FavoritesScreen(), 'title': 'Favorites'}
+  ];
+  int _currentPageIndex = 0;
+
+  void moveToPage(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0,
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text('Meals'),
-            bottom: TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              )
-            ]),
-          ),
-          body: TabBarView(children: [CategoriesScreen(), FavoritesScreen()])),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_currentPageIndex]['title']),
+      ),
+      body: _pages[_currentPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          onTap: moveToPage,
+          currentIndex: _currentPageIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category), label: 'Categories'),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites')
+          ]),
     );
   }
 }
